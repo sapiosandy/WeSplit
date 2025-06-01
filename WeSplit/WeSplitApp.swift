2b0b47204df9984d8514469ef812555db9dee12d
Sandy Gomez <sgomez1029@gmail.com>
2025-06-01 12:02:39 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sandra Gomez on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
