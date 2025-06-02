6c3c318fa1417ff6c199727d87a1d9b8f1954085
Sandy Gomez <sgomez1029@gmail.com>
2025-06-02 10:53:41 -0700

    Added done button to get rid of keypad

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10,15,20,25,0]
     var totalPerPerson: Double {
@@ -34,6 +35,7 @@ struct ContentView: View {
                         format: .currency(code: Locale.current.currency?.identifier ?? "USD")
                     )
                     .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -55,6 +57,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(Text("WeSplit"))
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
